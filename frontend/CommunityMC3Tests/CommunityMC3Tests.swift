cccb5395e7ccbacc99784fcf9f2f6f7254b5346d
bryanzanr <bryanzanr@gmail.com>
2020-07-08 10:13:09 +0700

    Testing merge the submodule

---
+++ frontend/CommunityMC3Tests/CommunityMC3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  CommunityMC3Tests.swift
+//  CommunityMC3Tests
+//
+//  Created by Bryanza on 06/07/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import XCTest
+@testable import CommunityMC3
+
+class CommunityMC3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
