b9bd5107d11003a73f066db865967e6c025f4468
bryanzanr <bryanzanr@gmail.com>
2020-07-20 01:20:18 +0700

    Add auth screen, cloudkit, and updated json database

---
+++ frontend/mobile/CommunityMC3/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginController.swift
+//  CommunityMC3
+//
+//  Created by Bryanza on 20/07/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
