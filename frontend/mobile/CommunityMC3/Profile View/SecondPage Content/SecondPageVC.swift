ea77752d36508f92c5119cbf7365f33280317f66
Theofani Samuel <liow.ts@gmail.com>
2020-07-27 15:21:16 +0700

    begin adding multiple VC in PageViewController

---
+++ frontend/mobile/CommunityMC3/Profile View/SecondPage Content/SecondPageVC.swift
@@ -0,0 +1,29 @@
+//
+//  SecondPageVC.swift
+//  CommunityMC3
+//
+//  Created by Theofani on 27/07/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+enum ShowcaseSection:Int{
+    case Music = 0
+    case Photos = 1
+    case Videos = 2
+    case Count = 3
+}
+
+class SecondPageVC: UIViewController {
+
+    @IBOutlet weak var showcaseTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        showcaseTableView.register(UINib(nibName: "ShowcaseTableCell", bundle: nil), forCellReuseIdentifier:"showcaseTableCell")
+        
+    }
+    
+
+}
