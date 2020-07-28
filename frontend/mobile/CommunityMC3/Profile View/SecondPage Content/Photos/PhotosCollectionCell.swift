97b7c67060435bb8f457bdf5de2f3af1ea43f48a
Theofani Samuel <liow.ts@gmail.com>
2020-07-28 10:59:57 +0700

    done layout user profile Showcase tab menu

---
+++ frontend/mobile/CommunityMC3/Profile View/SecondPage Content/Photos/PhotosCollectionCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotosCollectionCell.swift
+//  CommunityMC3
+//
+//  Created by Theofani on 28/07/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionCell: UICollectionViewCell {
+
+    @IBOutlet weak var photosCollectionImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
