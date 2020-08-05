e5ad05dc856fcd788d55276bb3f2c8ea26d63a80
bryanzanr <bryanzanr@gmail.com>
2020-08-06 01:41:51 +0700

    Update edit randomizer genre to setting genre with table view

--- frontend/mobile/CommunityMC3/Setting View/GenreProfileController.swift
+++ frontend/mobile/CommunityMC3/Setting View/GenreProfileController.swift
@@ -13,7 +13,8 @@ class GenreProfileController: UIViewController {
     var genreArray = ["Rock", "Jazz", "Pop", "RnB", "Acoutic", "Blues"]
     var genreTemp = [Int]()
     var selectedIndexGenreBool: [IndexPath: Bool] = [:]
-    @IBOutlet weak var genreProfile: UICollectionView!
+    
+    @IBOutlet weak var genreProfile: UITableView!
     @IBOutlet weak var applyButton: UIButton!
     @IBOutlet weak var editGenreLabel: UILabel!
     @IBOutlet weak var cancelButton: UIButton!
@@ -33,7 +34,8 @@ class GenreProfileController: UIViewController {
     }
     
     func setup(){
-        genreProfile.register(UINib(nibName: "GenreProfileCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "genreProfile")
+//        genreProfile.register(UINib(nibName: "GenreProfileCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "genreProfile")
+        genreProfile.register(UINib(nibName: "GenreProfileTableViewCell", bundle: nil), forCellReuseIdentifier: "genreProfileContextCell")
         genreProfile.allowsMultipleSelection = true
         
         applyButton.layer.cornerRadius = 5
@@ -44,13 +46,21 @@ class GenreProfileController: UIViewController {
     }
     
     @IBAction func clearAllGenreProfile(_ sender: UIButton) {
-        let selectedGenre = genreProfile.indexPathsForSelectedItems
+//        let selectedGenre = genreProfile.indexPathsForSelectedRows
+//
+//        for indexPath in selectedGenre! {
+//            let cell = genreProfile.cellForItem(at: indexPath) as! GenreProfileCollectionViewCell
+//            genreProfile.deselectItem(at: indexPath, animated: true)
+//            cell.genreProfileCheckbox.image = nil
+//            cell.genreProfileCheckbox.layer.borderWidth = 1
+//            genreTemp.removeAll()
+//        }
+        let selectedIndex = genreProfile.indexPathsForSelectedRows
         
-        for indexPath in selectedGenre! {
-            let cell = genreProfile.cellForItem(at: indexPath) as! GenreProfileCollectionViewCell
-            genreProfile.deselectItem(at: indexPath, animated: true)
-            cell.genreProfileCheckbox.image = nil
-            cell.genreProfileCheckbox.layer.borderWidth = 1
+        for i in selectedIndex! {
+            genreProfile.deselectRow(at: i, animated: true)
+            genreProfile.cellForRow(at: i)?.accessoryType = UITableViewCell.AccessoryType.none
+            
             genreTemp.removeAll()
         }
     }
@@ -62,35 +72,78 @@ class GenreProfileController: UIViewController {
     
 }
 
-extension GenreProfileController: UICollectionViewDataSource, UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//extension GenreProfileController: UICollectionViewDataSource, UICollectionViewDelegate {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return genreArray.count
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = genreProfile.dequeueReusableCell(withReuseIdentifier: "genreProfile", for: indexPath) as! GenreProfileCollectionViewCell
+//        cell.genreProfileLabel.text = genreArray[indexPath.row]
+//        cell.genreProfileCheckbox.layer.borderColor = UIColor.lightGray.cgColor
+//        cell.genreProfileCheckbox.layer.masksToBounds = true
+//        cell.genreProfileCheckbox.layer.borderWidth = 1
+//        cell.genreProfileCheckbox.layer.cornerRadius = 6
+//        
+//        return cell
+//    }
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        if genreTemp.contains(indexPath.row){
+//            let cell = genreProfile.cellForItem(at: indexPath) as! GenreProfileCollectionViewCell
+//            self.genreTemp.remove(at: self.genreTemp.firstIndex(of: indexPath.row)!)
+//            cell.genreProfileCheckbox.image = nil
+//            cell.genreProfileCheckbox.layer.borderWidth = 1
+//        }else{
+//            let cell = genreProfile.cellForItem(at: indexPath) as! GenreProfileCollectionViewCell
+//            self.genreTemp.append(indexPath.row)
+//            cell.genreProfileCheckbox.image = #imageLiteral(resourceName: "checkBox")
+//            cell.genreProfileCheckbox.layer.borderWidth = 0
+//        }
+//    }
+//    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+//        selectedIndexGenreBool[indexPath] = false
+//    }
+//}
+
+extension GenreProfileController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return genreArray.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = genreProfile.dequeueReusableCell(withReuseIdentifier: "genreProfile", for: indexPath) as! GenreProfileCollectionViewCell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "genreProfileContextCell", for: indexPath) as! GenreProfileTableViewCell
         cell.genreProfileLabel.text = genreArray[indexPath.row]
-        cell.genreProfileCheckbox.layer.borderColor = UIColor.lightGray.cgColor
-        cell.genreProfileCheckbox.layer.masksToBounds = true
-        cell.genreProfileCheckbox.layer.borderWidth = 1
-        cell.genreProfileCheckbox.layer.cornerRadius = 6
-        
         return cell
     }
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 35
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if genreTemp.contains(indexPath.row){
-            let cell = genreProfile.cellForItem(at: indexPath) as! GenreProfileCollectionViewCell
+            print("genre remove")
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.none
             self.genreTemp.remove(at: self.genreTemp.firstIndex(of: indexPath.row)!)
-            cell.genreProfileCheckbox.image = nil
-            cell.genreProfileCheckbox.layer.borderWidth = 1
         }else{
-            let cell = genreProfile.cellForItem(at: indexPath) as! GenreProfileCollectionViewCell
+            print("genre aadd")
             self.genreTemp.append(indexPath.row)
-            cell.genreProfileCheckbox.image = #imageLiteral(resourceName: "checkBox")
-            cell.genreProfileCheckbox.layer.borderWidth = 0
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.checkmark
         }
+        print("genre \(genreTemp)")
     }
-    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        selectedIndexGenreBool[indexPath] = false
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if genreTemp.contains(indexPath.row){
+            print("genre remove")
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.none
+            self.genreTemp.remove(at: self.genreTemp.firstIndex(of: indexPath.row)!)
+        }else{
+            print("genre add")
+            self.genreTemp.append(indexPath.row)
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.checkmark
+        }
+        print("genre \(genreTemp)")
     }
+    
 }
---
+++ frontend/mobile/CommunityMC3/Setting View/GenreTableView/GenreProfileTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  GenreProfileTableViewCell.swift
+//  Allegro
+//
+//  Created by Bryanza on 06/08/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class GenreProfileTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var genreProfileLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
