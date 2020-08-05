a4b40251dc305785013ffdf30c27135c1e8e99a1
Theofani Samuel <liow.ts@gmail.com>
2020-08-05 15:20:15 +0700

    edit layout Onboarding View

---
+++ frontend/mobile/CommunityMC3/Onboarding View/OnboardingCarouselView.swift
@@ -0,0 +1,77 @@
+//
+//  OnboardingCarouselView.swift
+//  Allegro
+//
+//  Created by Theofani on 05/08/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingCarouselView: UIPageViewController {
+
+    lazy var items: [UIViewController] = {
+        let sb = UIStoryboard(name: "Onboarding", bundle: nil)
+        
+        let vc1 = sb.instantiateViewController(withIdentifier: "onboardingView1")
+        let vc2 = sb.instantiateViewController(withIdentifier: "onboardingView2")
+        let vc3 = sb.instantiateViewController(withIdentifier: "onboardingView3")
+
+        
+        return [vc1, vc2, vc3]
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataSource = nil
+        self.delegate = nil
+    }
+
+}
+
+extension OnboardingCarouselView: UIPageViewControllerDataSource {
+    
+    func pageViewController(_: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = items.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return items.last
+        }
+        
+        guard items.count > previousIndex else {
+            return nil
+        }
+        
+        return items[previousIndex]
+    }
+    
+    func pageViewController(_: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = items.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        guard items.count != nextIndex else {
+            return items.first
+        }
+        
+        guard items.count > nextIndex else {
+            return nil
+        }
+        
+        return items[nextIndex]
+    }
+    
+    func presentationIndex(for _: UIPageViewController) -> Int {
+        guard let firstViewController = viewControllers?.first,
+            let firstViewControllerIndex = items.firstIndex(of: firstViewController) else {
+                return 0
+        }
+        
+        return firstViewControllerIndex
+    }
+}
--- frontend/mobile/CommunityMC3/Onboarding View/OnboardingView.swift
+++ frontend/mobile/CommunityMC3/Onboarding View/OnboardingView.swift
@@ -9,22 +9,30 @@
 import UIKit
 
 class OnboardingView: UIViewController {
-
+    
+    var onboardingVC: OnboardingCarouselView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "onboardingViewSegue"
+        {
+            print("onboardingSegue")
+            onboardingVC = (segue.destination as! OnboardingCarouselView)
+        }
     }
-    */
-
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
