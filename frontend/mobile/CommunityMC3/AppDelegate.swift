b0c7857ddc81f18193d8b82b729fefb2ac30ae9b
Theofani Samuel <liow.ts@gmail.com>
2020-08-06 11:21:24 +0700

    begin add Onboarding to App delegate

--- frontend/mobile/CommunityMC3/AppDelegate.swift
+++ frontend/mobile/CommunityMC3/AppDelegate.swift
@@ -13,12 +13,34 @@ import CloudKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    
+    var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        DataManager.shared()
+        let launchedBefore = UserDefaults.standard.bool(forKey: "hasLaunched")
+        self.window = UIWindow(frame: UIScreen.main.bounds)
         
+        let launchStoryboard = UIStoryboard(name: "OnboardingScreen", bundle: nil)
+        
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        var vc: UIViewController
+        
+        if launchedBefore
+        {
+            vc = mainStoryboard.instantiateInitialViewController()!
+        }
+        else
+        {
+            vc = launchStoryboard.instantiateViewController(identifier: "onboardingStoryboard")
+        }
+        
+        UserDefaults.standard.set(true, forKey: "hasLaunched")
+        
+        self.window?.rootViewController = vc
+        self.window?.makeKeyAndVisible()
+        
+        DataManager.shared()
         return true
     }
     
