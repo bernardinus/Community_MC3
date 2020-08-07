986533a7f17e0da7a7939811d4e0d959eaf6f359
Rommy Julius Dwidharma <62103999+rommyjulius@users.noreply.github.com>
2020-08-07 10:43:19 +0700

    Fix Layout on Mini Track Player

--- frontend/mobile/CommunityMC3/Main Storyboard/MiniTrackPlayerController.swift
+++ frontend/mobile/CommunityMC3/Main Storyboard/MiniTrackPlayerController.swift
@@ -7,13 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
-class MiniTrackPlayerController: UIViewController {
+protocol MiniTrackPlayerDelegate: class {
+    func play(_ trackURL: String?)
+    func stop()
+}
 
+class MiniTrackPlayerController: UIViewController, AVAudioPlayerDelegate, MiniTrackPlayerDelegate {
+
+    @IBOutlet weak var trackProgressView: UIProgressView!
+    @IBOutlet weak var trackTitleLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var coverTrackImage: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+    
+    var trackPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+    }
+    
+    func play(_ trackURL: String?) {
+        var error: NSError? = nil
+        let audioPath = Bundle.main.path(forResource: trackURL, ofType: "mp3")
+        do{
+            trackPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+        } catch let error1 as NSError{
+            error = error1
+        }
+        trackPlayer!.delegate = self
+        
+        if error == nil {
+            trackPlayer?.delegate = self
+            trackPlayer?.prepareToPlay()
+            trackPlayer?.play()
+        }
+    }
+    
+    func stop(){
+        trackPlayer?.stop()
     }
     
 }
---
+++ frontend/mobile/CommunityMC3/Manager/TrackManager.swift
@@ -0,0 +1,19 @@
+//
+//  TrackManager.swift
+//  Allegro
+//
+//  Created by Rommy Julius Dwidharma on 07/08/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import Foundation
+
+class TrackManager {
+    
+    static let shared = TrackManager()
+    init() {}
+    
+    
+    
+    
+}
